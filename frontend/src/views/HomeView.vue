69b97328821d1576736ebcc69fd95793a3257489
SquiddyPoos <liuwenkai2017@gmail.com>
2022-05-11 23:56:51 +0800

    port frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,27 @@
-<script lang="ts" setup>
-import TheWelcome from '@/components/TheWelcome.vue'</script>
-
 <template>
-  <main>
-    <TheWelcome/>
-  </main>
+  <div class="main">
+    <h1>Hey! A website!</h1>
+  </div>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+.main {
+  display: inline-flex;
+  width: 100%;
+  justify-content: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+
+.main > h1 {
+  font-family: 'Source Sans Pro', sans-serif;
+}
+</style>
+
+<style>
+body {
+  background-color: lightblue;
+}
+</style>
