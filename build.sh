e70554a6c4466d499f856d6b5f356397bf9fbc3a
SquiddyPoos <liuwenkai2017@gmail.com>
2022-05-12 13:15:54 +0800

    >:( installed arm64 debian on raspi

--- build.sh
+++
@@ -1,5 +0,0 @@
-# Workaround to prevent docker-compose parallel builds
-# (which doesn't work due to npm being choked by bandwidth limits)
-
-docker compose build backend
-docker compose build frontend
