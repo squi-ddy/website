d145f3750cad448eb84ede8055102d44c41f5ccb
SquiddyPoos <liuwenkai2017@gmail.com>
2022-11-07 02:18:32 +0800

    add cors to api
    # Conflicts:
    #	backend/pnpm-lock.yaml

--- backend/src/api/api.www.ts
+++ backend/src/api/api.www.ts
@@ -3,9 +3,12 @@ import API from "./types/api"
 import supervend from "./supervend/supervend"
 import astroview from "./astroview/astroview"
 import lcs from "./lcs/lcs"
+import cors from "cors"
 
 const apiRouter = express.Router()
 const apis: API[] = [supervend, astroview, lcs]
+
+apiRouter.use(cors())
 
 for (const api of apis) {
     apiRouter.use(`/${api.info.link}`, api.router)
--- backend/src/api/lcs/api.ts
+++ backend/src/api/lcs/api.ts
@@ -143,7 +143,6 @@ lcsRouter.get("/", async (_req, res) => {
     if (lcs === null) {
         handleQueryError(new Error("DB error"), res)
     } else {
-        delete (lcs as { sus?: Meaning }).sus
         res.json(lcs)
     }
 })
@@ -156,29 +155,6 @@ lcsRouter.get("/:id(\\d+)", async (req, res) => {
     } else if (lcs === null) {
         handleQueryError(new Error("DB error"), res)
     } else {
-        delete (lcs as { sus?: Meaning }).sus
-        res.json(lcs)
-    }
-})
-
-lcsRouter.get("/us", async (_req, res) => {
-    const lcs = await getLCS()
-    if (lcs === null) {
-        handleQueryError(new Error("DB error"), res)
-    } else {
-        delete (lcs as { s?: Meaning }).s
-        res.json(lcs)
-    }
-})
-
-lcsRouter.get("/us/:id(\\d+)", async (req, res) => {
-    const lcs = await getLCSByIndex(parseInt(req.params.id || "0"))
-    if (lcs === undefined) {
-        res.status(404).send("Invalid LCS id")
-    } else if (lcs === null) {
-        handleQueryError(new Error("DB error"), res)
-    } else {
-        delete (lcs as { s?: Meaning }).s
         res.json(lcs)
     }
 })
