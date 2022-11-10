204a6cb35890987c2427ff6d30d4917e2cfb9e56
SquiddyPoos <liuwenkai2017@gmail.com>
2022-11-11 00:18:41 +0800

    move cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,14 @@ import express from "express"
 import bodyParser from "body-parser"
 import compression from "compression"
 import helmet from "helmet"
+import cors from "cors"
 
 const app = express()
 const port = settings.PORT
 const domain = settings.DOMAIN
 const server = http.createServer(app)
 
+app.use(cors())
 app.use(helmet())
 app.use(compression())
 app.use(bodyParser.json())
