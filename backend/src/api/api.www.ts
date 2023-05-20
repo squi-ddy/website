97fcdb58114ccbc7da1243b90ca6a405068ea1d9
SquiddyPoos <liuwenkai2017@gmail.com>
2023-05-21 01:23:15 +0800

    remove body-parser

--- backend/src/api/api.www.ts
+++ backend/src/api/api.www.ts
@@ -4,10 +4,8 @@ import supervend from "./supervend/supervend"
 import astroview from "./astroview/astroview"
 import lcs from "./lcs/lcs"
 import prompt_artistry from "./prompt-artistry/prompt-artistry"
-import cookieParser from "cookie-parser"
 
 const apiRouter = express.Router()
-apiRouter.use(cookieParser())
 const apis: API[] = [supervend, astroview, lcs, prompt_artistry]
 
 for (const api of apis) {
@@ -17,13 +15,6 @@ for (const api of apis) {
 apiRouter.get("/", (_req, res): void => {
     res.json({
         apis: apis,
-    })
-})
-
-apiRouter.get("/ctf", (_req, res): void => {
-    console.log(_req.cookies)
-    res.json({
-        a:1
     })
 })
 
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,6 @@ import { settings } from "./util/env/settings"
 import { apiRouter } from "./api/api.www"
 import http from "http"
 import express from "express"
-import bodyParser from "body-parser"
 import compression from "compression"
 import helmet from "helmet"
 import cors from "cors"
@@ -15,7 +14,7 @@ const server = http.createServer(app)
 app.use(cors())
 app.use(helmet())
 app.use(compression())
-app.use(bodyParser.json({ limit: "50mb" }))
+app.use(express.json({ limit: "50mb" }))
 
 app.use("/", apiRouter)
 
