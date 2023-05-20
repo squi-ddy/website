9be7bdbfb38c480a20d3515f7a6046174cc57254
SquiddyPoos <liuwenkai2017@gmail.com>
2023-05-21 00:53:13 +0800

    lmao

--- backend/src/api/api.www.ts
+++ backend/src/api/api.www.ts
@@ -4,8 +4,10 @@ import supervend from "./supervend/supervend"
 import astroview from "./astroview/astroview"
 import lcs from "./lcs/lcs"
 import prompt_artistry from "./prompt-artistry/prompt-artistry"
+import cookieParser from "cookie-parser"
 
 const apiRouter = express.Router()
+apiRouter.use(cookieParser())
 const apis: API[] = [supervend, astroview, lcs, prompt_artistry]
 
 for (const api of apis) {
@@ -15,6 +17,13 @@ for (const api of apis) {
 apiRouter.get("/", (_req, res): void => {
     res.json({
         apis: apis,
+    })
+})
+
+apiRouter.get("/ctf", (_req, res): void => {
+    console.log(_req.cookies)
+    res.json({
+        a:1
     })
 })
 
